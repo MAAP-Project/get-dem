8b4e4366ec4a2f4011c524b7f1ef0862ec57f971
Chuck Daniels <chuck@developmentseed.org>
2024-02-28 15:08:16 -0500

    Support dev dependencies

--- build-env.sh
+++ build-env.sh
@@ -3,6 +3,28 @@
 set -eou pipefail
 
 basedir=$(dirname "$(readlink -f "$0")")
+dev=0
+conda_args=()
 
-set -x  # echo on
-PIP_REQUIRE_VENV=0 conda env update --solver libmamba --file "${basedir}"/environment.yml "$@"
+while ((${#})); do
+    case "${1}" in
+    --dev)
+        dev=1
+        ;;
+    *)
+        conda_args+=("${1}")
+        ;;
+    esac
+
+    shift
+done
+
+set -x # echo on
+PIP_REQUIRE_VENV=0 conda env update --prune --solver libmamba --file "${basedir}"/environment.yml "$@"
+set +x # echo off
+
+if [ "${dev}" -eq 1 ]; then
+    set -x # echo on
+    PIP_REQUIRE_VENV=0 conda env update --solver libmamba --file "${basedir}"/environment-dev.yml "$@"
+    set +x # echo off
+fi

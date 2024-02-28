cd01fe9c462e2815dfbd9826a8f33b16d9fcbdc3
Chuck Daniels <chuck@developmentseed.org>
2024-02-28 11:54:08 -0500

    Add maap-py dev dependency

--- build-env.sh
+++ build-env.sh
@@ -5,4 +5,4 @@ set -eou pipefail
 basedir=$(dirname "$(readlink -f "$0")")
 
 set -x  # echo on
-conda env update --solver libmamba --file "${basedir}"/environment.yml "$@"
+PIP_REQUIRE_VENV=0 conda env update --solver libmamba --file "${basedir}"/environment.yml "$@"

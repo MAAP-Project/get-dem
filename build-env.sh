6958a5f303035b1af14c8dbacffc513bb051f8a0
Chuck Daniels <chuck@developmentseed.org>
2024-03-07 22:57:35 -0500

    Install DEM ipykernel

--- build-env.sh
+++ build-env.sh
@@ -19,12 +19,21 @@ while ((${#})); do
     shift
 done
 
+prefix="${CONDA_ROOT:-${HOME}/.conda}/envs/dem"
+
 set -x # echo on
-PIP_REQUIRE_VENV=0 conda env update --quiet --prune --solver libmamba --file "${basedir}"/environment.yml "$@"
+PIP_REQUIRE_VENV=0 conda env update --prefix "${prefix}" \
+    --quiet --prune --solver libmamba --file "${basedir}"/environment.yml \
+    "${conda_args[@]}"
 set +x # echo off
 
 if [ "${dev}" -eq 1 ]; then
     set -x # echo on
-    PIP_REQUIRE_VENV=0 conda env update --quiet --solver libmamba --file "${basedir}"/environment-dev.yml "$@"
+    PIP_REQUIRE_VENV=0 conda env update --prefix "${prefix}" \
+        --quiet --solver libmamba --file "${basedir}"/environment-dev.yml \
+        "${conda_args[@]}"
+    "${CONDA_EXE:-conda}" run --prefix "${prefix}" \
+        python -Xfrozen_modules=off -m ipykernel install \
+        --user --name dem --display-name "Get DEM (Python)"
     set +x # echo off
 fi

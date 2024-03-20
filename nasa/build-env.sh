801154eea4c1a997b51879c22df924f0465a8af2
Sujen Shah <sujen1412@gmail.com>
2024-03-20 07:46:16 -0700

    Update inputs for building OGC package.

--- nasa/build-env.sh
+++ nasa/build-env.sh
@@ -3,7 +3,7 @@
 basedir=$( cd "$(dirname "$0")" ; pwd -P )
 REPO_ROOT_PATH=$(dirname ${basedir})
 
-conda config --set solver libmamba
+# Not required from NASA MAAP base image versions >=3.1.4
+#conda config --set solver libmamba
 
 conda env create -f ${REPO_ROOT_PATH}/environment.yaml
-
